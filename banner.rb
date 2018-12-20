e5f998bd32f8104dc1b2c6544e9c374040eee8ea
MattHelton <mjoelhelton@me.com>
2018-12-19 21:52:39 -0500

    Adds name, hello, and languages array/iterator

--- banner.rb
+++ banner.rb
@@ -1,7 +1,17 @@
-# hello
 
-# name
+name = 'Matt Helton'
 
-# language array and itterator
+hello = "Hello World! My name is #{name}"
+
+puts hello
+
+# language array and iterator
+languages = ['Ruby', 'JavaScript', 'HTML5', 'CSS']
+
+languages.each do |language|
+    puts language
+end
+
+# frameworks and libraries array and iterator
 
 # skills array and itterator
