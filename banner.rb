23a49e88a753b619b26e002614d25e7edd0afdc7
MattHelton <mjoelhelton@me.com>
2018-12-26 21:47:45 -0500

    Adds list_handler method to iterate over any list it is given as an argument.

--- banner.rb
+++ banner.rb
@@ -3,28 +3,24 @@ name = 'Matt Helton'
 
 
 
+def list_handler(list)
+    list.each do |x|
+        return x
+    end
+end
+
+
+
 
 
 # language array and iterator
 languages = ['Ruby', 'JavaScript', 'HTML5', 'CSS']
-
-introduction = languages.each do |language|
-    print language + ' '
-end
-
-
-
-
-puts introduction
-
-
+language_list = list_handler(languages)
 # frameworks and libraries array and iterator
 frameworks_and_libraries = ['React', 'Rails', 'Bootstrap', 'Rspec', 'Chai', 'Redux']
 # skills array and itterator
-frameworks_and_libraries.each do |x|
-    puts x
-end
+frameworks_and_libraries_list = list_handler(frameworks_and_libraries)
 
-hello = "Hello World! My name is #{name}, and I love #{introduction}"
+hello = "Hello World! My name is #{name}, and I love #{language_list} Especially when they let me use #{frameworks_and_libraries_list}"
 
 puts hello
