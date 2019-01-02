13ab3886b795fbaf31b60227eea0044dd5fe9ede
MattHelton <mjoelhelton@me.com>
2019-01-01 21:03:07 -0500

    Calls list)handler methods inside of puts instead of saving them as variables

--- banner.rb
+++ banner.rb
@@ -13,12 +13,11 @@ end
 
 # language array and iterator
 languages = ['Ruby', 'JavaScript', 'HTML5', 'CSS']
-language_list = list_handler(languages)
+
 # frameworks and libraries array and iterator
 frameworks_and_libraries = ['React', 'Rails', 'Bootstrap', 'Rspec', 'Chai', 'Redux']
-# skills array and itterator
-frameworks_and_libraries_list = list_handler(frameworks_and_libraries)
 
-hello = "Hello World! My name is #{name}, and I love #{language_list.join(", and ")}! Especially when they give me an excuse to use #{frameworks_and_libraries_list.join(" or ")} or all of them in the same project!"
+
+hello = "Hello World! My name is #{name}, and I love #{list_handler(languages).join(", and ")}! Especially when they give me an excuse to use #{list_handler(frameworks_and_libraries).join(" or ")} or all of them in the same project!"
 
 puts hello
